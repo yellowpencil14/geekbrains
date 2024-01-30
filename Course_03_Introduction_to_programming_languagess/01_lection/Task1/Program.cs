ce05127e53071180047af371e7be94438ae754e9
Yellow Pencil <petpavser@mail.ru>
2024-01-30 18:09:06 +0300

    add some variables 01_lection\Task1

--- Course_03_Introduction_to_programming_languagess/01_lection/Task1/Program.cs
+++ Course_03_Introduction_to_programming_languagess/01_lection/Task1/Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("Hello, world");
+﻿// Console.WriteLine("Hello, world");
+
+int value = 100;
+Console.WriteLine(value);
+
+double d = 5.55;
+char c = '!';
+bool b = true;
+Console.WriteLine(d);
+Console.WriteLine(c);
+Console.WriteLine(b);
