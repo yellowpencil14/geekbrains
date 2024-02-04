febb09d2dc66023641b8d5e148a6692098c93299
Yellow Pencil <petpavser@mail.ru>
2024-02-05 00:42:52 +0300

    Did C:\geekbrains\Course_03_Introduction_to_programming_languagess\02_seminar\Homework02. Something went wrong. You have to debug it later. It's time to go to bed now, bro

---
+++ Course_03_Introduction_to_programming_languagess/02_seminar/Homework02/Program.cs
@@ -0,0 +1,59 @@
+﻿// Максимальное из трех чисел
+// Инструкция по использованию платформы
+// Внутри класса Answer напишите метод FindMax, который принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+
+using System;
+
+public class Answer {
+   static int FindMax(int a, int b, int c)
+   {
+        // Введите свое решение ниже
+
+        if (a > b)
+        {
+            if (a > c)
+            {
+                System.Console.WriteLine(a);
+            }
+            else
+            {
+                System.Console.WriteLine(c);
+            }
+        }
+        else
+        {
+            if (b > c)
+            {
+                System.Console.WriteLine(b);
+            }
+            else
+            {
+                System.Console.WriteLine(c);
+            }
+        }
+    }
+
+// Не удаляйте и не меняйте метод Main! 
+    static public void Main(string[] args) {
+        int a, b, c;
+
+        if (args.Length >= 3)
+        {
+            a = int.Parse(args[0]);
+            b = int.Parse(args[1]);
+            c = int.Parse(args[2]);
+        }
+        else
+        {
+            // Здесь вы можете поменять значения для отправки кода на Выполнение
+            a = 5;
+            b = 6;
+            c = 7;
+        }
+
+        // Не удаляйте строки ниже
+        int result = FindMax(a, b, c);
+        System.Console.WriteLine($"{result}");
+    }
+}
