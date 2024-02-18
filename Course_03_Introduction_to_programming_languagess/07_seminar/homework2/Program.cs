1b395dae66cbe888947733261009720b01403d95
Yellow Pencil <petpavser@mail.ru>
2024-02-18 18:14:26 +0300

    done Course_03_Introduction_to_programming_languagess\07_seminar\homework2

--- Course_03_Introduction_to_programming_languagess/07_seminar/homework2/Program.cs
+++ Course_03_Introduction_to_programming_languagess/07_seminar/homework2/Program.cs
@@ -5,3 +5,28 @@
 Напишите программу, которая покажет количество чётных чисел в массиве.
 */
 
+
+void inputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(100, 1000); // [100; 999]
+}
+
+
+Console.Clear();
+
+Console.Write("Введите количество элементов массива: ");
+int n = int.Parse(Console.ReadLine()!);
+
+int[] array = new int[n];
+inputArray(array);
+Console.WriteLine($"Начальный массив: [{string.Join(", ", array)}]");
+
+int count = 0;
+foreach (int element in array)
+{
+    if (element % 2 == 0)
+        count = count + 1;
+}
+
+Console.WriteLine($"В массиве {count} чётных элементов");
