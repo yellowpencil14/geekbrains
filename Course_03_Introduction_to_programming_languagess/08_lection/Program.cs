45d5b16102e5843271ffb15ff9de4b5fba4f583b
Yellow Pencil <petpavser@mail.ru>
2024-02-20 22:36:30 +0300

    done Course_03_Introduction_to_programming_languagess\08_lection

--- Course_03_Introduction_to_programming_languagess/08_lection/Program.cs
+++ Course_03_Introduction_to_programming_languagess/08_lection/Program.cs
@@ -138,4 +138,157 @@ int[,] table3 =
 Вывести на экран «интересные» элементы массива.
 */
 
-РЕШЕНИЕ ЗАДАЧИ И ПРОДОЛЖЕНИЕ ЛЕКЦИИ СМ. 00:36:00
+// int[,] createMatrix (int rowCount, int columnCount)
+// {
+//     int[,] matrix = new int[rowCount, columnCount];
+
+//     Random rnd = new Random();
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             matrix[i, j] = rnd.Next(1, 1000);
+//         }
+//     }
+//     return matrix;
+// }
+
+// bool isInteresting(int value)
+// {
+//     int sumOfDigits = getSumOfDigits(value);
+//     if (sumOfDigits %2 == 0)
+//         return true;
+//     return false;
+// }
+
+// int getSumOfDigits(int value)
+// {
+//     int sum = 0;
+//     while (value > 0)
+//     {
+//         sum = sum + value % 10;
+//         value = value / 10;
+//     }
+//     return sum;
+// }
+
+// void showMatrix(int[,] matrix)
+// {
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             Console.Write($"{matrix[i, j]} ");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+
+
+// int[,] matrix = createMatrix(3, 4);
+
+// showMatrix(matrix);
+
+// foreach (int e in matrix)
+// {
+//     if (isInteresting(e) == true)
+//         Console.WriteLine(e);
+// }
+
+
+
+/*
+Строки в C#
+
+Строка - последовательный набор символов, который используется для представления текста.
+Строка в языке C# - массив символов типа char.
+Для работы со строками в C# используется специальный тип - string.
+
+
+Характеристики строк:
+
+- доступ к элементам строки может быть осуществлён операцией индексации
+Пример.
+string str = "Hello, world!";
+Console.Write(str[0]); // -> H
+Console.Write(str[1]); // -> e
+    - элементы строки не могут быть изменены
+    - размер существующей строки не может быть изменён
+
+
+Создание строк в C#:
+
+    Способ 1:
+string s1 = "hello";
+
+    Способ 2:
+char[] ch_array = { 'w', 'o', 'r', 'l', 'd' };
+string s2 = new String(ch_array); // String - конструктор строки. Конструктор - спец. метод, который создаёт объект
+
+    Способ 3:
+string s3 = new String('a', 6); // -> "aaaaaa"
+
+
+Методы строк в C#:
+- Contains - определяет, содержится ли подстрока в строке
+- EndsWith - определяет, совпадает ли конец строки с подстрокой
+- IndexOf - находит индекс первого вхождения символа или подстроки в строке
+- Insert - вставляет в строку подстроку
+- Replace - замещает в строке символ или подстроку другим символом или подстрокой
+*/
+
+
+/*
+Задача №3
+
+Считать с консоли строку, состоящую из цифр и латинских букв.
+Сформировать новую строку, состоящую из букв исходной строки.
+*/
+
+// string getLetters(string s)
+// {
+//     string letters = "";
+//     foreach (char e in s)
+//     {
+//         if (char.IsAsciiLetter(e) == true)
+//         {
+//             letters = letters + e;
+//         }
+//     }
+//     return letters;
+// }
+
+
+// string str = Console.ReadLine()!;
+// string result = getLetters(str);
+// Console.WriteLine(result);
+
+
+/*
+Операторы передачи управления:
+
+- break - прерывает выполнение ближайшего внешнего цикла (while, for, foreach).
+    Часто используется, когда возникло условие, при котором цикл нужно немедленно прервать.
+    Распространённый случай - остановка цикла с условием true (бесконечный цикл).
+    Пример.
+    while (true)
+    {
+        Console.WriteLine("I am in loop!");
+        break;
+    }
+
+- continue - начинает новую итерацию ближайшего внешнего цикла (while, for, foreach).
+    Часто используется, когда возникло условие, при клтором оставшиеся инструкции тела цикла нужно пропустить.
+    Пример.
+    for (int i = 1; i < 5; i++)
+    {
+        Console.Write($"Ineration {i}: ");
+        if (i == 3)
+        {
+            Console.WriteLine("Skip");
+            continue;
+        }
+        Console.WriteLine("Some processing");
+    }
+*/
