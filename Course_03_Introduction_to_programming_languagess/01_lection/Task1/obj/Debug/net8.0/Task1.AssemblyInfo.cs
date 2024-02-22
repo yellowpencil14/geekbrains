4196f9cb4569877ec0cbdf95bcca2d3aa8dda6a1
Yellow Pencil <petpavser@mail.ru>
2024-02-22 09:03:48 +0300

    change settings vs code

--- Course_03_Introduction_to_programming_languagess/01_lection/Task1/obj/Debug/net8.0/Task1.AssemblyInfo.cs
+++ Course_03_Introduction_to_programming_languagess/01_lection/Task1/obj/Debug/net8.0/Task1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12faa387162c36c3acb5ae741b6d39865003aa3e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+632dceda17c50c23d5990bae4168cbf4e447aed1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
