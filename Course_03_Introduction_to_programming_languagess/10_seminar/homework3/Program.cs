e5f6ab1372420cb1acb2b4e04f0258b938d1728d
Yellow Pencil <petpavser@mail.ru>
2024-03-03 21:33:26 +0300

    Done homework from Course_03_Introduction_to_programming_languagess\10_seminar

--- Course_03_Introduction_to_programming_languagess/10_seminar/homework3/Program.cs
+++ Course_03_Introduction_to_programming_languagess/10_seminar/homework3/Program.cs
@@ -6,3 +6,22 @@
 "шалаш" -> Да
 "55655" -> Да
 */
+
+Console.Clear();
+
+Console.Write("Введите строку: ");
+string str = Console.ReadLine()!;
+
+int result = 0;
+for (int i = 0; i < str.Length / 2; i++)
+{
+	if (str[i] == str[str.Length - i - 1])
+	{
+		result += 1;
+	}
+}
+
+if (result > 0)
+	Console.Write("Строка является палиндромом");
+else
+	Console.Write("Строка не является палиндромом");
--- Course_03_Introduction_to_programming_languagess/10_seminar/homework4/Program.cs
+++ Course_03_Introduction_to_programming_languagess/10_seminar/homework4/Program.cs
@@ -4,3 +4,21 @@
 
 "Hello my world" -> "world my Hello"
 */
+
+Console.Clear();
+
+Console.Write("Введите строку, состоящую из слов с пробелами: ");
+string str = Console.ReadLine()!;
+
+string[] words = str.Split(' '); // делаем из введенной строки массив из элементов string, в котором элементами являются слова строки (разделенные пробелом символы)
+
+for (int i = 0; i < words.Length / 2; i++) // разворачиваем массив
+{
+	string temp = words[i];
+	words[i] = words[words.Length - i - 1];
+	words[words.Length - i - 1] = temp;
+}
+
+string reversedStr = string.Join(" ", words); // делаем обратно массив строкой, разделяя элементы пробелами
+
+Console.Write($"Перевернутая строка: {reversedStr}");
