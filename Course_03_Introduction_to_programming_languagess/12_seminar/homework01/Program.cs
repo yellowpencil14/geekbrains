6839f681ffdb55ef9777d1890c9aeae900f6efa4
Yellow Pencil <petpavser@mail.ru>
2024-03-08 23:18:24 +0300

    done homework1, 2, 3 from Course_03_Introduction_to_programming_languagess\12_seminar (attestation)

--- Course_03_Introduction_to_programming_languagess/12_seminar/homework01/Program.cs
+++ Course_03_Introduction_to_programming_languagess/12_seminar/homework01/Program.cs
@@ -6,6 +6,29 @@
 
 
 Пример:
-M = 1; N = 5 -> "1, 2, 3, 4, 5"
-M = 4; N = 8 -> "4, 5, 6, 7, 8"
+M = 1; N = 5 -> "1 2 3 4 5"
+M = 4; N = 8 -> "4 5 6 7 8"
 */
+
+string printNumbers(int num1, int num2)
+{
+	if (num1 > num2) // чтобы поменять числа местами, если нужно 
+	{
+		int temp = num2;
+		num2 = num1;
+		num1 = temp;
+	}
+	if (num2 == num1) // базовый случай
+		return $"{num1} ";
+	return printNumbers(num1, num2 - 1) + $"{num2} ";
+}
+
+Console.Clear();
+
+Console.Write("Введите первое число: ");
+int num1 = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите второе число: ");
+int num2 = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine(printNumbers(num1, num2));
--- Course_03_Introduction_to_programming_languagess/12_seminar/homework02/Program.cs
+++ Course_03_Introduction_to_programming_languagess/12_seminar/homework02/Program.cs
@@ -7,3 +7,23 @@
 Пример:
 m = 2, n = 3 -> A(m,n) = 29
 */
+
+int calculateAckermann(int n, int m)
+{
+	if (n == 0) // базовый случай
+		return m + 1;
+	else if (m == 0)
+		return calculateAckermann(n - 1, 1);
+	else
+		return calculateAckermann(n - 1, calculateAckermann(n, m - 1));
+}
+
+Console.Clear();
+
+Console.Write("Введите первое число (n): ");
+int num1 = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите второе число (m): ");
+int num2 = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine($"A(n, m) = {calculateAckermann(num1, num2)}"); // в примере дан неверный порядок переменных m и n. Я сделал задание согласно статье из Википедии.
